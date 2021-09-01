012bb7a5b86b705a838b775e1dae420b0a09dd72
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2021-08-31 20:23:18 -0400

    Change feature

---
+++ script/main.js
@@ -0,0 +1,12 @@
+
+const token = 'rQC1sA1Fi7ALbrWVbK9ADEr64fSS3EYD'; // Utiliser votre propre clé
+const keyword = 'hot';
+const grid = document.querySelector('.grid');
+
+fetch(`https://api.giphy.com/v1/gifs/random?api_key=${token}&tag=${keyword}&rating=G`)
+.then(response => response.json())
+.then(result => { 
+  console.log(result);
+
+  grid.innerHTML = `<img src="${result.data.images.original.url}">`;
+});
