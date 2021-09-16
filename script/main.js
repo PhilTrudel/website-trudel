b31f1e648950216fccfaddcf2d029e16d0976d9c
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2021-09-15 21:28:12 -0400

    deleted js code for gif

--- script/main.js
+++ script/main.js
@@ -3,14 +3,6 @@ const token = 'rQC1sA1Fi7ALbrWVbK9ADEr64fSS3EYD'; // Utiliser votre propre clé
 const keyword = 'hot';
 const grid = document.querySelector('.grid');
 
-fetch(`https://api.giphy.com/v1/gifs/random?api_key=${token}&tag=${keyword}&rating=G`)
-.then(response => response.json())
-.then(result => { 
-  console.log(result);
-
-  grid.innerHTML = `<img src="${result.data.images.original.url}">`;
-});
-
 
 //animation GSAP
 gsap.timeline().to(arrow, 
